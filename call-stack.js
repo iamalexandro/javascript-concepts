60e83df362bf36bb7021ecb3c6cbd451a4a1c61e
Nicola Di Candia <nicoladicandiajaimes@gmail.com>
2023-03-15 11:51:15 -0500

    adding call stack concept

---
+++ call-stack.js
@@ -0,0 +1,24 @@
+const cortar = (ingrediente) => {
+  console.log("Cortar " + ingrediente);
+};
+
+const mezclarIngredientes = (n) => {
+  if (n <= 0) return;
+
+  console.log("Mezclar #" + n);
+  mezclarIngredientes(n - 1);
+};
+
+const comer = () => {
+  console.log("Comer");
+};
+
+const hacerEnsaladaMixta = () => {
+  cortar("tomate");
+  cortar("lechuga");
+  cortar("cebolla");
+  mezclarIngredientes(5);
+  comer();
+};
+
+hacerEnsaladaMixta();
